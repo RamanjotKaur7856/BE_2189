fd49cb1401d8138f038502742f268e93b3650f90
RamanjotKaur7856 <156178826+RamanjotKaur7856@users.noreply.github.com>
2025-01-21 10:01:50 +0530

    Assignment 1

---
+++ Assignments/Assignment 1/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.use((req, res, next) => {
+    const logDetails = {
+        timestamp: new Date().toISOString(),
+        ip: req.ip,
+        url: req.originalUrl,
+        protocol: req.protocol,
+        method: req.method,
+        hostname: req.hostname,
+    };
+
+    fs.appendFile('requests.log', JSON.stringify(logDetails) + '\n', (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err);
+        }
+    });
+
+    console.log(`${req.method} request for ${req.url}`);
+    console.log(`Request IP: ${req.ip}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
